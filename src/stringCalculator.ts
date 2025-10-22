d2ade21646b821295700858ff53ad06f7313bb4a
RodricHil <rodrick.hilarian@proaccuracy.com>
2025-10-22 11:42:21 +0530

    step-3 : handle two commaseparated numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,14 @@
 export function add(numbers: string): number {
   // step 1 & 2
+  //   if (!numbers) return 0;
+  //   return Number(numbers);
+
+  // step 3
   if (!numbers) return 0;
-  return Number(numbers);
+  const parts = String(numbers).split(",");
+  if (parts.length !== 2) {
+    throw new Error("Only two numbers are allowed.");
+  }
+  const nums = parts.map((n) => Number(n));
+  return nums.reduce((a, b) => a + b, 0);
 }

a84198caa94a9ab6cca6d05ec3c524a78a9963dd
RodricHil <rodrick.hilarian@proaccuracy.com>
2025-10-22 12:07:59 +0530

    step-7 : throw error for negative numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -25,24 +25,44 @@ export function add(numbers: string): number {
   //   return nums.reduce((a, b) => a + b, 0);
   //   Example : "5\n10\n15" , "1\n2,3"
 
-
   // step 6 (support custom delimiter //;\n syntax)
+  //   if (!numbers) return 0;
+
+  //   let delimiter = /,|\n/;
+  //   let nums = numbers;
+
+  //   if (numbers.startsWith("//")) {
+  //     const parts = String(numbers).split("\n");
+  //     const custom = parts[0].substring(2);
+  //     // Escape regex special characters
+  //     delimiter = new RegExp(custom.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
+  //     nums = parts[1];
+  //   }
+
+  //   const values = nums.split(delimiter).map((n) => Number(n));
+  //   return values.reduce((a, b) => a + b, 0);
+
+  //   Example: add("//;\n1;2;3;4");
+
+  // step 7 (throw error for negative numbers)
   if (!numbers) return 0;
 
   let delimiter = /,|\n/;
   let nums = numbers;
 
   if (numbers.startsWith("//")) {
-    const parts = numbers.split("\n");
+    const parts = String(numbers).split("\n");
     const custom = parts[0].substring(2);
-    // Escape regex special characters
-    delimiter = new RegExp(custom.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
+    delimiter = new RegExp(custom);
     nums = parts[1];
   }
 
-  const values = nums.split(delimiter).map((n) => Number(n));
-  return values.reduce((a, b) => a + b, 0);
+  const arr = nums.split(delimiter).map((n) => Number(n));
+  const negatives = arr.filter((n) => n < 0);
+  if (negatives.length)
+    throw new Error(`negatives not allowed: ${negatives.join(",")}`);
 
-//   Example: add("//;\n1;2;3;4");
-  
+  return arr.reduce((a, b) => a + b, 0);
+
+//   Example: add("//#\n1#2#-3#4");
 }

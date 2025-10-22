f4f7d3a391568b38db984a7bf0d1f312239bc4ea
RodricHil <rodrick.hilarian@proaccuracy.com>
2025-10-22 11:46:05 +0530

    step-4 : handle unknown number of inputs

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -4,11 +4,17 @@ export function add(numbers: string): number {
   //   return Number(numbers);
 
   // step 3
-  if (!numbers) return 0;
+//   if (!numbers) return 0;
+//   const parts = String(numbers).split(",");
+//   if (parts.length !== 2) {
+//     throw new Error("Only two numbers are allowed.");
+//   }
+//   const nums = parts.map((n) => Number(n));
+//   return nums.reduce((a, b) => a + b, 0);
+
+// step 4
+   if (!numbers) return 0;
   const parts = String(numbers).split(",");
-  if (parts.length !== 2) {
-    throw new Error("Only two numbers are allowed.");
-  }
   const nums = parts.map((n) => Number(n));
   return nums.reduce((a, b) => a + b, 0);
 }

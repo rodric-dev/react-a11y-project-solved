08329e1499fb6672759dfe707896f6b499a6b8ac
RodricHil <rodrick.hilarian@proaccuracy.com>
2025-10-22 11:54:33 +0530

    step-5 : support newline as delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,20 +1,27 @@
 export function add(numbers: string): number {
-  // step 1 & 2
+  // step 1 & 2 (empty string returns 0 and single number returns itself)
   //   if (!numbers) return 0;
   //   return Number(numbers);
 
-  // step 3
-//   if (!numbers) return 0;
-//   const parts = String(numbers).split(",");
-//   if (parts.length !== 2) {
-//     throw new Error("Only two numbers are allowed.");
-//   }
-//   const nums = parts.map((n) => Number(n));
-//   return nums.reduce((a, b) => a + b, 0);
+  // step 3 (handle two commaseparated numbers)
+  //   if (!numbers) return 0;
+  //   const parts = String(numbers).split(",");
+  //   if (parts.length !== 2) {
+  //     throw new Error("Only two numbers are allowed.");
+  //   }
+  //   const nums = parts.map((n) => Number(n));
+  //   return nums.reduce((a, b) => a + b, 0);
 
-// step 4
-   if (!numbers) return 0;
-  const parts = String(numbers).split(",");
+  // step 4 (handle unknown number of inputs)
+  //   if (!numbers) return 0;
+  //   const parts = String(numbers).split(",");
+  //   const nums = parts.map((n) => Number(n));
+  //   return nums.reduce((a, b) => a + b, 0);
+
+  // step 5 (support newline as delimiter)
+  if (!numbers) return 0;
+  const parts = String(numbers).split(/,|\n/);
   const nums = parts.map((n) => Number(n));
   return nums.reduce((a, b) => a + b, 0);
+//   Example : "5\n10\n15" , "1\n2,3"
 }

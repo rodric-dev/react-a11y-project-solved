211a37dc15fb0d5c87b48b4a6b330f056f913d82
RodricHil <rodrick.hilarian@proaccuracy.com>
2025-10-22 12:03:58 +0530

    step-6 : support custom delimiter //;\n syntax

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -19,9 +19,30 @@ export function add(numbers: string): number {
   //   return nums.reduce((a, b) => a + b, 0);
 
   // step 5 (support newline as delimiter)
+  //   if (!numbers) return 0;
+  //   const parts = String(numbers).split(/,|\n/);
+  //   const nums = parts.map((n) => Number(n));
+  //   return nums.reduce((a, b) => a + b, 0);
+  //   Example : "5\n10\n15" , "1\n2,3"
+
+
+  // step 6 (support custom delimiter //;\n syntax)
   if (!numbers) return 0;
-  const parts = String(numbers).split(/,|\n/);
-  const nums = parts.map((n) => Number(n));
-  return nums.reduce((a, b) => a + b, 0);
-//   Example : "5\n10\n15" , "1\n2,3"
+
+  let delimiter = /,|\n/;
+  let nums = numbers;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    const custom = parts[0].substring(2);
+    // Escape regex special characters
+    delimiter = new RegExp(custom.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
+    nums = parts[1];
+  }
+
+  const values = nums.split(delimiter).map((n) => Number(n));
+  return values.reduce((a, b) => a + b, 0);
+
+//   Example: add("//;\n1;2;3;4");
+  
 }
